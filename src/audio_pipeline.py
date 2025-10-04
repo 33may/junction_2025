dc5c53a64a7d798a061b588d5c808893e5d2f857
may33 <eternalmay33@gmail.com>
2025-10-04 13:46:13 +0200

    test push ssh

--- src/audio_pipeline.py
+++ src/audio_pipeline.py
@@ -1,5 +1,28 @@
+import json
+import time
+
+import torch
 import whisper
 
 print(whisper.__version__)
 
-# WhisperModel = whisper.load_model("medium")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(device)
+
+model = whisper.load_model("medium", device=device)
+
+audio_file = "../data/Want to become fluent in Eglish.mp3"
+
+print("start processing")
+
+time_start = time.time()
+
+result = model.transcribe(audio_file)
+
+print(f"processed time: {time.time() - time_start}")
+
+with open("../data/outputs/transcription.json", "w", encoding="utf-8") as f:
+    json.dump(result, f, ensure_ascii=False, indent=2)
+
+print(result["text"])
